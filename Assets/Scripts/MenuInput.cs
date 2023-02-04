af7ecbccce04616910ab6772d9138d6120ed1feb
dwexel <88518920+dwexel@users.noreply.github.com>
2023-02-04 14:34:22 -0600

    add main menu

---
+++ Assets/Scripts/MenuInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInput : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // demo scene
+        SceneManager.LoadScene(1);
+    }
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
