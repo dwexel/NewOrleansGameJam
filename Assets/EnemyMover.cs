cd5d8ae2133e06040a884d6817c6c5a5ad6336dc
dwexel <88518920+dwexel@users.noreply.github.com>
2023-02-04 14:10:33 -0600

    enemy prefab with navmesh

--- Assets/EnemyMover.cs
+++ Assets/EnemyMover.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMover : MonoBehaviour
 {
+    public GameObject TargetObj;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = TargetObj.transform.position;
+
+
     }
 
     // Update is called once per frame
